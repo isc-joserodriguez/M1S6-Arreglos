8eec837768fbc06462407f3c6586f6c181dbb566
José Rodriguez <isc.joserodriguez@gmail.com>
2021-08-28 09:39:24 -0600

    Sort & Filter

--- main.js
+++ main.js
@@ -22,15 +22,49 @@ for (var elemento of arreglo) {
 console.log(arreglo)
 console.log('ForEach');
 
-var concatenacion='';
+var concatenacion = '';
 
-arreglo.forEach(function (elemento,indice) {
-    if(indice==2){
-        console.log('El indice 2 contiene: '+ elemento);
-    }else{
-        console.log(elemento);
+function miFuncion(auto, index) {
+    if (index == 2) {
+        console.log('El indice 2 contiene: ' + auto);
+    } else {
+        console.log(auto, index);
     }
-    concatenacion=concatenacion+elemento;
+    concatenacion = concatenacion + auto;
+}
+
+arreglo.forEach(miFuncion);
+
+/* arreglo.forEach(function (auto, index) {
+    if (index == 2) {
+        console.log('El indice 2 contiene: ' + auto);
+    } else {
+        console.log(auto);
+    }
+    concatenacion = concatenacion + auto;
+});  */
+
+/* console.log(concatenacion); */
+
+//Sort
+
+var nums = [1, 2, 3, 4, 5, 10, 20, 30, 100, 1000, 200, 60000];
+
+nums.sort(function (primerElemento, segundoElemento) {
+    //Algoritmo que evalue los elementos.
+    return segundoElemento - primerElemento;
 });
 
-console.log(concatenacion);
+//Filter
+function impares(numero) {
+    /* if (numero % 2 !== 0) {
+        return true;
+    } else {
+        return false;
+    } */
+
+    return numero % 2 !== 0;
+}
+var numerosImpares=nums.filter(impares);
+
+console.log(numerosImpares);
